3092d88fe310990843b011a30865376d6609a053
Dtctv129 <ah.belagha@esi-sba.dz>
2024-01-11 16:52:30 +0100

    cors

--- main.py
+++ main.py
@@ -16,12 +16,10 @@ load_dotenv()
 app = FastAPI()
 
 
-origins = [
-    "http://localhost:3000",  
-]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  # You can restrict to specific headers if needed
